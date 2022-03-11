cd144943cea1fd5309b2408b683d78a3fb595a21
文享旭 <846212939@qq.com>
2022-03-11 21:55:30 +0800

    init

---
+++ src/main/java/design/DesignPatternsLearnApplication.java
@@ -0,0 +1,13 @@
+package design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternsLearnApplication.class, args);
+    }
+
+}
